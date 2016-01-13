c4de51f147d40f26ed9f0407279ba7674b03c2cc
Owen Berry <owen.berry@gmail.com>
2016-01-13 15:08:41 -0500

    Global confifguration for SQS message processing

---
+++ src/main/java/jenkins/plugins/slack/mq/GlobalConfig.java
@@ -0,0 +1,126 @@
+package jenkins.plugins.slack.mq;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import hudson.Extension;
+import hudson.util.FormValidation;
+import hudson.util.Secret;
+import jenkins.model.GlobalConfiguration;
+import jenkins.plugins.slack.SlackListener;
+import net.sf.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+@Extension
+public class GlobalConfig extends GlobalConfiguration {
+	
+	private static final Logger logger = Logger.getLogger(GlobalConfig.class.getName());
+
+    private String slackSqsQueue;
+    private String slackAwsAccessKeyId;
+    private Secret slackAwsSecretAccessKey;
+    private String slackTokenForSqsIntegration;
+
+    public GlobalConfig() {
+        load();
+    }
+
+    public FormValidation doCheckSlackSqsQueue(@QueryParameter String value) {
+        if (StringUtils.isBlank(value)) {
+            return FormValidation.warning("Please set a queue name or URL");
+        }
+        return FormValidation.ok();
+    }
+    
+    public FormValidation doCheckSlackAwsAccessKeyId(@QueryParameter String value) {
+        if (StringUtils.isBlank(value)) {
+            return FormValidation.warning("Please set an access key id");
+        }
+        return FormValidation.ok();
+    }
+    
+    public FormValidation doCheckSlackAwsSecretAccessKey(@QueryParameter String value) {
+        if (StringUtils.isBlank(value)) {
+            return FormValidation.warning("Please set a secret access key");
+        }
+        return FormValidation.ok();
+    }
+    
+    public FormValidation doCheckSlackTokenForSqsIntegration(@QueryParameter String value) {
+        if (StringUtils.isBlank(value)) {
+            return FormValidation.warning("Please set a token from Slack integration");
+        }
+        return FormValidation.ok();
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json)
+            throws FormException {
+        req.bindJSON(this, json);
+        save();
+        return true;
+    }
+
+    public FormValidation doTestConnection(
+            @QueryParameter("slackSqsQueue") final String sqsQueue,
+            @QueryParameter("slackAwsAccessKeyId") final String awsAccessKeyId,
+            @QueryParameter("slackAwsSecretAccessKey") final Secret awsSecretAccessKey)
+            throws FormException {
+    	
+		if (StringUtils.isBlank(sqsQueue)
+				|| StringUtils.isBlank(awsAccessKeyId)
+				|| StringUtils.isBlank(Secret.toString(awsSecretAccessKey))) {
+			return FormValidation
+					.error("Please provide all configuration values");
+		}
+        
+    	try {
+            SqsProfile profile = new SqsProfile(awsAccessKeyId, awsSecretAccessKey, sqsQueue);
+            String queue = profile.getQueueUrl();
+            if(queue != null) {
+                return FormValidation.ok("Verified SQS Queue " + queue);
+            } else {
+                return FormValidation.error("Failed to validate the account");
+            }
+        } catch (RuntimeException ex) {
+        	logger.log(Level.WARNING, "Failed to validate the Amazon SQS configuration: " + ex.getMessage());
+            return FormValidation.error("Failed to validate the account. Check the Jenkins log files.");
+        }
+    }
+
+	public String getSlackSqsQueue() {
+		return slackSqsQueue;
+	}
+
+	public void setSlackSqsQueue(String slackSqsQueue) {
+		this.slackSqsQueue = slackSqsQueue;
+	}
+
+	public String getSlackAwsAccessKeyId() {
+		return slackAwsAccessKeyId;
+	}
+
+	public void setSlackAwsAccessKeyId(String slackAwsAccessKeyId) {
+		this.slackAwsAccessKeyId = slackAwsAccessKeyId;
+	}
+
+	public Secret getSlackAwsSecretAccessKey() {
+		return slackAwsSecretAccessKey;
+	}
+
+	public void setSlackAwsSecretAccessKey(Secret slackAwsSecretAccessKey) {
+		this.slackAwsSecretAccessKey = slackAwsSecretAccessKey;
+	}
+
+	public String getSlackTokenForSqsIntegration() {
+		return slackTokenForSqsIntegration;
+	}
+
+	public void setSlackTokenForSqsIntegration(
+			String slackTokenForSqsIntegration) {
+		this.slackTokenForSqsIntegration = slackTokenForSqsIntegration;
+	}
+}
---
+++ src/main/java/jenkins/plugins/slack/mq/SqsProfile.java
@@ -0,0 +1,91 @@
+/**
+ * Code originally from https://github.com/jenkinsci/github-sqs-plugin
+ */
+package jenkins.plugins.slack.mq;
+
+import hudson.model.AbstractDescribableImpl;
+import hudson.util.Secret;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+
+/**
+ * SqsProfile to access SQS
+ *
+ * @author aaronwalker
+ */
+public class SqsProfile extends AbstractDescribableImpl<SqsProfile> implements AWSCredentials{
+
+    public final String awsAccessKeyId;
+    public final Secret awsSecretAccessKey;
+    public final String sqsQueue;
+
+    static final String queueUrlRegex = "^https://sqs\\.(.+?)\\.amazonaws\\.com/(.+?)/(.+)$";
+    static final Pattern endpointPattern = Pattern.compile("(sqs\\..+?\\.amazonaws\\.com)");
+    private final boolean urlSpecified;
+    private AmazonSQS client;
+
+
+    @DataBoundConstructor
+    public SqsProfile(String awsAccessKeyId, Secret awsSecretAccessKey, String sqsQueue) {
+        this.awsAccessKeyId = awsAccessKeyId;
+        this.awsSecretAccessKey = awsSecretAccessKey;
+        this.sqsQueue = sqsQueue;
+        this.urlSpecified = Pattern.matches(queueUrlRegex, sqsQueue);
+        this.client = null;
+    }
+
+    public String getAWSAccessKeyId() {
+        return awsAccessKeyId;
+    }
+
+    public String getAWSSecretKey() {
+        return awsSecretAccessKey.getPlainText();
+    }
+
+    public AmazonSQS getSQSClient() {
+        if(client == null) {
+            client =  new AmazonSQSClient(this);
+            if(urlSpecified) {
+                Matcher endpointMatcher = endpointPattern.matcher(getSqsQueue());
+                if(endpointMatcher.find()) {
+                    String endpoint = endpointMatcher.group(1);
+                    client.setEndpoint(endpoint);
+                }
+            }
+        }
+        return client;
+    }
+
+    public String getSqsQueue() {
+        return sqsQueue;
+    }
+
+    public String getQueueUrl() {
+        return urlSpecified ? sqsQueue
+                            : createQueue(getSQSClient(), sqsQueue);
+    }
+
+    /**
+     * Create a Amazon SQS queue if it does already exists
+     * @param sqs  Amazon SQS client
+     * @param queue the name of the queue
+     * @return  the queue url
+     */
+    private String createQueue(AmazonSQS sqs, String queue) {
+        for(String url : sqs.listQueues().getQueueUrls()) {
+            if(url.endsWith("/" + queue)) {
+                return url;
+            }
+        }
+        //The queue wasn't found so we will create it
+        return sqs.createQueue(new CreateQueueRequest(queue)).getQueueUrl();
+    }
+}
